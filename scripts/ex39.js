f2b18ff1ce432469bd018c4808719f623f0b8ccc
Dev Mello <robertale45@gmail.com>
2025-03-20 09:34:29 -0300

    ex39 , html , css , js

---
+++ scripts/ex39.js
@@ -0,0 +1,13 @@
+let input = document.getElementById('name');
+
+let btn = document.getElementById('btn')
+
+btn.addEventListener("click", ()=>{
+// e.preventDefault();
+
+let nomeDig = input.value;
+
+    alert(`Olá ${nomeDig}`);
+
+
+});
