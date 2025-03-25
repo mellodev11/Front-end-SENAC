9c637151443f9e63630a40eef704ed90de95b92b
Dev Mello <robertale45@gmail.com>
2025-03-25 10:36:52 -0300

    refatorando lógica

--- scripts/ex49.js
+++ scripts/ex49.js
@@ -2,7 +2,7 @@ let n1 = document.getElementById('num1');
         
         let btn = document.getElementById('btn');
         let resultado = document.getElementById('demo');
-       
+        
 
 
         btn.addEventListener("click",(e)=>{
@@ -11,17 +11,20 @@ let n1 = document.getElementById('num1');
            let valor = parseFloat(n1.value);
            
           
-           if(valor < 4.9){
+           if(valor < 4.99 ){
                resultado.innerHTML = `Reprovado`
                
             }else if(valor < 7 ){
-                resultado.innerHTML = `recuperação`
+                resultado.innerHTML = `Recuperação`
+                
+            }else if(valor>10){
+                resultado.innerHTML = `Valor invalido`
                 
             }else{
-                resultado.innerHTML = `Aprovado`
-                
-           }
+               resultado.innerHTML = `Aprovado`
 
+           }
+        
 
         }
         )
--- scripts/ex50.js
+++ scripts/ex50.js
@@ -1,12 +1,13 @@
 let n1 = document.getElementById('num1');
 let n2 = document.getElementById('num2');
+const form = document.getElementById('myForm');
         
         let btn = document.getElementById('btn');
         let resultado = document.getElementById('demo');
        
 
 
-        btn.addEventListener("click",(e)=>{
+        form.addEventListener("submit",(e)=>{
            e.preventDefault();
     
            let valor1 = parseFloat(n1.value);
