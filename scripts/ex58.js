10602cc78e305f41a9306380dcfa97b80d194d0d
Dev Mello <robertale45@gmail.com>
2025-03-27 11:40:30 -0300

    começando ex59

---
+++ scripts/ex58.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('myForm');
+const btn = document.getElementById('btn');
+let demo = document.querySelector('#demo');
+
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+   
+
+    demo.style.fontSize = `20px`
+       
+    
+});
